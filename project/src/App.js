002838f6b158aead3ff6241bac2aec36672a912c
zhaoqiao6526163com <52695566+zhaoqiao6526163com@users.noreply.github.com>
2019-10-21 15:14:15 +0800

    init

---
+++ project/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+import Rouetr from './router'
+function App() {
+  return (
+    <div className="App">
+      <Rouetr></Rouetr>
+    </div>
+  );
+}
+
+export default App;
---
+++ project/src/admin/admin.js
@@ -0,0 +1,22 @@
+import React,{Component} from 'react'
+import './admin.less'
+import Nav from '../nav/nav'
+class Admin extends Component{
+  render(){
+    return(
+      <div className="admin">
+        <div className="admin-nav">
+          <Nav/>
+        </div>
+        <div className='admin-right'>
+          <div className="header">??</div>
+          <div className="middle">
+            {this.props.children}
+          </div>
+          <div className="footer">??</div>
+        </div>
+      </div>
+    )
+  }
+}
+export default Admin
