0a4e314ca3b19aa7c0c5d53c23294556fc76ba21
zhaoqiao6526163com <52695566+zhaoqiao6526163com@users.noreply.github.com>
2019-10-24 09:18:31 +0800

    ini

--- project/src/component/visitor.js
+++ project/src/component/visitor.js
@@ -1,16 +1,42 @@
 import React,{Component} from 'react'
-
+import {Card} from 'antd'
+import ReactEcharts from 'echarts-for-react'
 class Visitor extends Component{
   constructor(){
     super()
     this.state={
-      
+      // option:{
+      //   legend: {
+      //       orient: 'vertical',
+      //       left: 'left',
+      //       data: ['????','????','????','????','????']
+      //   },
+      //   series : [
+      //       {
+      //           name: '????',
+      //           type: 'pie',
+      //           radius : '55%',
+      //           center: ['50%', '60%'],
+      //           data:[
+      //               {value:335, name:'????'},
+      //               {value:310, name:'????'},
+      //               {value:234, name:'????'},
+      //               {value:135, name:'????'},
+      //               {value:1548, name:'????'}
+      //           ],
+                
+      //       }
+      //   ]
+    // }
+    
     }
   }
   render(){
     return(
       <div>
-         用户分布
+         <Card>
+          <ReactEcharts option={this.state.option} />
+         </Card>
       </div>
     )
   }
